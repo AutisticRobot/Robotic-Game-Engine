284902da795d8a78ecff95f604e272cbec42cbf6
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-09-10 21:35:16 -0700

    replaced build.sh with makefile

--- cppPrototype/build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build
-mkdir build
-cp -r resources build
-cd build
-g++ -c ../RGEVulLin64.cpp ../rendering.cpp
-g++ RGEVulLin64.o rendering.o -o RGEBuild -lsfml-graphics -lsfml-window -lsfml-system
-rm -f RGEVulLin64.o
-rm -f rendering.o
-./RGEBuild
