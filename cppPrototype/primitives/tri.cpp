a8646c9aef31d9e36e3eabaa88f97461995c8890
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-09-15 20:26:24 -0700

    fixed tri calculation error

--- cppPrototype/primitives/tri.cpp
+++ cppPrototype/primitives/tri.cpp
@@ -13,6 +13,13 @@ tri::tri()
     triangle[1].position = sf::Vector2f(500.f, 50.f);
     triangle[2].position = sf::Vector2f(850.f, 400.f);
 
+    triangle[0].position.x *= ratio.x;
+    triangle[1].position.x *= ratio.x;
+    triangle[2].position.x *= ratio.x;
+
+    triangle[0].position.y *= ratio.y;
+    triangle[1].position.y *= ratio.y;
+    triangle[2].position.y *= ratio.y;
 
 
     // define the default color of the triangle's points
@@ -24,12 +31,21 @@ tri::tri()
 
 int tri::updateRatio(sf::Vector2f inRatio)
 {
+    triangle[0].position.x /= ratio.x;
+    triangle[1].position.x /= ratio.x;
+    triangle[2].position.x /= ratio.x;
+
+    triangle[0].position.y /= ratio.y;
+    triangle[1].position.y /= ratio.y;
+    triangle[2].position.y /= ratio.y;
+
+    std::cout << "part1: " << std::endl;
+    std::cout << ratio.x << std::endl;
+    std::cout << triangle[0].position.x << std::endl;
+
     ratio.x = 300.f / inRatio.x;
     ratio.y = 200.f / inRatio.y;
     // define the position of the triangle's points
-    triangle[0].position = sf::Vector2f(150.f, 400.f);
-    triangle[1].position = sf::Vector2f(500.f, 50.f);
-    triangle[2].position = sf::Vector2f(850.f, 400.f);
 
     triangle[0].position.x *= ratio.x;
     triangle[1].position.x *= ratio.x;
@@ -39,6 +55,9 @@ int tri::updateRatio(sf::Vector2f inRatio)
     triangle[1].position.y *= ratio.y;
     triangle[2].position.y *= ratio.y;
 
+    std::cout << "part2: " << std::endl;
+    std::cout << ratio.x << std::endl;
+    std::cout << triangle[0].position.x << std::endl;
 
 
     return 0;
