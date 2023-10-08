923445bc6ef7fd202ccb89fbd6924892eac84d20
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-10-07 23:55:34 -0700

    started work on logger

---
+++ cppPrototype/cprint.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+template<typename T>
+int cprint(T test, bool con = true, std::string foreHex = "#000000", std::string backHex = "#999999");
