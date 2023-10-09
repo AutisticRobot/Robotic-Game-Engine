603edd2eda6996972160f29633f49cd325c4f980
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-10-08 23:39:27 -0700

    added inital cprint code draft

---
+++ cppPrototype/cprint.cpp
@@ -0,0 +1,11 @@
+#include "cprint.hpp"
+
+
+template<typename T>
+int cprint(T test, bool con, std::string foreHex, std::string backHex)
+{
+    if(con)
+    {
+        std::cout << test << std::endl;
+    }
+}
