7687bc83888d990d5936e8dc1cb6c2f1fcc26974
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-11-17 21:31:44 -0800

    started work planning node

--- headers/nodes/node.hpp
+++ headers/nodes/node.hpp
@@ -4,6 +4,14 @@ class node
 {
     public:
 
-        gli::Vector2<float> pos;
+        node(gli::Vector2<float> pos);
+
+        void push(gli::Vector2<float> direction, float force);
+
+        gli::Vector2<float> pos; //Position
+        gli::Vector2<float> vel; //Veloctiy
+        gli::Vector2<float> mass;//Mass (weight)
+
+    private:
 
 };
